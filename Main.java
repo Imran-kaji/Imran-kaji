a637f4e8ede0c2b86ac35feb8a4b954b384f6f50
imran-kaji <mdimrankaji216@gmail.com>
2021-10-27 19:43:41 +0530

    first

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main (String[]arg){
+
+        System.out.println("Welcome to BCET");
+        System.out.println("CSE");
+    }
+
+}
